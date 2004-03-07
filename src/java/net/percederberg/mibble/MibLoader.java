d60467018d345eb89cc53164d5561360f86ff159
Per Cederberg <per@percederberg.net>
2004-03-07 22:17:07 +0000

    Added support for unloading MIBs from the MibLoader.

--- src/java/net/percederberg/mibble/MibLoader.java
+++ src/java/net/percederberg/mibble/MibLoader.java
@@ -44,46 +44,46 @@ import java.util.ArrayList;
 
 /**
  * A MIB file loader. This class contains a search path for locating
- * MIB files, and also holds a refererence to previously loaded MIB 
- * files to avoid loading the same file multiple times. The MIB 
- * search path consists of directories with MIB files that can be 
+ * MIB files, and also holds a refererence to previously loaded MIB
+ * files to avoid loading the same file multiple times. The MIB
+ * search path consists of directories with MIB files that can be
  * imported into another MIB. If an import isn't found in the search
  * path, the default IANA and IETF MIB directories are searched. Note
  * that these files are normally stored as resources together with
  * the compiled code.<p>
- * 
- * The MIB loader is not thread-safe, i.e. it cannot be used 
- * concurrently in multiple threads. 
+ *
+ * The MIB loader is not thread-safe, i.e. it cannot be used
+ * concurrently in multiple threads.
  *
  * @author   Per Cederberg, <per at percederberg dot net>
- * @version  2.2
+ * @version  2.3
  * @since    2.0
  */
 public class MibLoader {
 
     /**
      * The base resource directories. These are used when locating a
-     * MIB stored as a resource inside the JAR file. 
+     * MIB stored as a resource inside the JAR file.
      */
-    private static final String[] RESOURCE_BASE = 
+    private static final String[] RESOURCE_BASE =
         { "mibs/iana", "mibs/ietf" };
-    
+
     /**
-     * The MIB file search path. This is a list of directories to 
-     * search for MIB files. If a MIB isn't found among these 
+     * The MIB file search path. This is a list of directories to
+     * search for MIB files. If a MIB isn't found among these
      * directories, the default resource paths will be attempted.
      */
     private ArrayList dirs = new ArrayList();
 
     /**
-     * The MIB files loaded. This list contains all MIB file loaded 
-     * with this loader, in order to avoid loading some MIB files 
+     * The MIB files loaded. This list contains all MIB file loaded
+     * with this loader, in order to avoid loading some MIB files
      * multiple times (and thus duplicating import symbols).
      */
     private ArrayList mibs = new ArrayList();
 
     /**
-     * The queue of MIB files to load. This queue contains file 
+     * The queue of MIB files to load. This queue contains file
      * objects.
      */
     private ArrayList queue = new ArrayList();
@@ -102,7 +102,7 @@ public class MibLoader {
     /**
      * Adds a directory to the MIB search path. If the directory
      * specified is null, the current working directory will be added.
-     * 
+     *
      * @param dir            the directory to add
      */
     public void addDir(File dir) {
@@ -113,10 +113,10 @@ public class MibLoader {
             dirs.add(dir);
         }
     }
-    
+
     /**
-     * Adds directories to the MIB search path. 
-     * 
+     * Adds directories to the MIB search path.
+     *
      * @param dirs           the directories to add
      */
     public void addDirs(File[] dirs) {
@@ -129,7 +129,7 @@ public class MibLoader {
      * Adds a directory and all subdirectories to the MIB search path.
      * If the directory specified is null, the current working
      * directory (and subdirectories) will be added.
-     * 
+     *
      * @param dir            the directory to add
      */
     public void addAllDirs(File dir) {
@@ -149,7 +149,7 @@ public class MibLoader {
 
     /**
      * Removes a directory from the MIB search path.
-     * 
+     *
      * @param dir            the directory to remove
      */
     public void removeDir(File dir) {
@@ -165,7 +165,7 @@ public class MibLoader {
 
     /**
      * Resets this loader. This means that all references to previuos
-     * MIB files will be removed, forcing a reload of any imported 
+     * MIB files will be removed, forcing a reload of any imported
      * MIB.
      */
     public void reset() {
@@ -177,7 +177,7 @@ public class MibLoader {
     /**
      * Returns the default MIB context. This context contains the
      * symbols that are predefined for all MIB:s (such as 'iso').
-     * 
+     *
      * @return the default MIB context
      */
     public MibContext getDefaultContext() {
@@ -188,9 +188,9 @@ public class MibLoader {
      * Returns a previously loaded MIB file. If the MIB file hasn't
      * been loaded, null will be returned. The MIB is identified by
      * it's MIB name (i.e. the module name).
-     * 
+     *
      * @param name           the MIB (module) name
-     * 
+     *
      * @return the MIB module if found, or
      *         null otherwise
      */
@@ -205,18 +205,18 @@ public class MibLoader {
         }
         return null;
     }
-    
+
     /**
      * Returns all previously loaded MIB files. If no MIB files have
      * been loaded an empty array will be returned.
-     * 
+     *
      * @return an array with all loaded MIB files
-     * 
+     *
      * @since 2.2
      */
     public Mib[] getAllMibs() {
         Mib[]  res;
-        
+
         res = new Mib[mibs.size()];
         mibs.toArray(res);
         return res;
@@ -224,13 +224,13 @@ public class MibLoader {
 
     /**
      * Searches for a MIB in the search path. The name specified
-     * should be the MIB file name, possibly leaving out the 
+     * should be the MIB file name, possibly leaving out the
      * extension. If the MIB isn't found in the directory search path,
      * the base resource path is also tested.
-     * 
+     *
      * @param name           the MIB file name (without extension)
-     * 
-     * @return the MIB found, or 
+     *
+     * @return the MIB found, or
      *         null if no MIB was found
      */
     private MibSource locate(String name) {
@@ -257,46 +257,46 @@ public class MibLoader {
 
     /**
      * Loads a MIB file. The file is searched for in the MIB search
-     * path. The MIB is identified by it's MIB name (i.e. the module 
-     * name). This method will also load all imported MIB:s if not 
+     * path. The MIB is identified by it's MIB name (i.e. the module
+     * name). This method will also load all imported MIB:s if not
      * previously loaded by this loader.
-     * 
+     *
      * @param name           the MIB name (filename without extension)
-     * 
+     *
      * @return the MIB file loaded
-     * 
-     * @throws FileNotFoundException if the MIB file couldn't be 
+     *
+     * @throws FileNotFoundException if the MIB file couldn't be
      *             found in the MIB search path
-     * @throws MibLoaderException if the MIB file couldn't be loaded 
+     * @throws MibLoaderException if the MIB file couldn't be loaded
      *             correctly
      */
     public Mib load(String name)
         throws FileNotFoundException, MibLoaderException {
 
         MibSource  src;
-        
+
         src = locate(name);
         if (src == null) {
-            throw new FileNotFoundException("couldn't locate MIB: '" + 
+            throw new FileNotFoundException("couldn't locate MIB: '" +
                                             name + "'");
         }
         return load(src);
     }
-    
+
     /**
-     * Loads a MIB file. This method will also load all imported 
+     * Loads a MIB file. This method will also load all imported
      * MIB:s if not previously loaded by this loader.
-     * 
+     *
      * @param file           the MIB file
-     * 
+     *
      * @return the MIB file loaded
-     * 
-     * @throws FileNotFoundException if the MIB file couldn't be 
+     *
+     * @throws FileNotFoundException if the MIB file couldn't be
      *             found
      * @throws MibLoaderException if the MIB file couldn't be loaded
      *             correctly
      */
-    public Mib load(File file) 
+    public Mib load(File file)
         throws FileNotFoundException, MibLoaderException {
 
         return load(new MibSource(file));
@@ -305,13 +305,13 @@ public class MibLoader {
     /**
      * Loads a MIB. This method will also load all imported MIB:s if
      * not previously loaded by this loader.
-     * 
+     *
      * @param src            the MIB source
-     * 
+     *
      * @return the MIB loaded
-     * 
+     *
      * @throws FileNotFoundException if the MIB couldn't be found
-     * @throws MibLoaderException if the MIB couldn't be loaded 
+     * @throws MibLoaderException if the MIB couldn't be loaded
      *             correctly
      */
     private Mib load(MibSource src)
@@ -331,22 +331,105 @@ public class MibLoader {
     }
 
     /**
+     * Unloads a MIB. This method will remove the loader reference to
+     * a previously loaded MIB if no other MIBs are depending on it.
+     * This method does not free the memory used by the MIB, but only
+     * releases all the loader references (thereby allowing the
+     * garbage collector to recover the memory used by the MIB).
+     *
+     * @param name           the MIB name
+     *
+     * @throws MibLoaderException if the MIB couldn't be unloaded
+     *             due to dependencies from other loaded MIBs
+     *
+     * @see #reset
+     *
+     * @since 2.3
+     */
+    public void unload(String name) throws MibLoaderException {
+        int  pos = mibs.indexOf(name);
+
+        if (pos >= 0) {
+            unload((Mib) mibs.get(pos));
+        }
+    }
+
+    /**
+     * Unloads a MIB. This method will remove the loader reference to
+     * a previously loaded MIB if no other MIBs are depending on it.
+     * This method does not free the memory used by the MIB, but only
+     * releases all the loader references (thereby allowing the
+     * garbage collector to recover the memory used by the MIB).
+     *
+     * @param file           the MIB file
+     *
+     * @throws MibLoaderException if the MIB couldn't be unloaded
+     *             due to dependencies from other loaded MIBs
+     *
+     * @see #reset
+     *
+     * @since 2.3
+     */
+    public void unload(File file) throws MibLoaderException {
+        int  pos = mibs.indexOf(file);
+
+        if (pos >= 0) {
+            unload((Mib) mibs.get(pos));
+        }
+    }
+
+    /**
+     * Unloads a MIB. This method will remove the loader reference to
+     * a previously loaded MIB if no other MIBs are depending on it.
+     * This method does not free the memory used by the MIB, but only
+     * releases all the loader references (thereby allowing the
+     * garbage collector to recover the memory used by the MIB).
+     *
+     * @param mib            the MIB
+     *
+     * @throws MibLoaderException if the MIB couldn't be unloaded
+     *             due to dependencies from other loaded MIBs
+     *
+     * @see #reset
+     *
+     * @since 2.3
+     */
+    public void unload(Mib mib) throws MibLoaderException {
+        Mib     referer;
+        String  message;
+        int     pos;
+
+        pos = mibs.indexOf(mib);
+        if (pos >= 0) {
+            for (int i = 0; i < mibs.size(); i++) {
+                referer = (Mib) mibs.get(i);
+                if (referer.getImport(mib.getName()) != null) {
+                    message = "cannot be unloaded due to reference in " +
+                              referer;
+                    throw new MibLoaderException(mib.getFile(), message);
+                }
+            }
+            mibs.remove(pos);
+        }
+    }
+
+    /**
      * Schedules the loading of a MIB file. The file is added to the
-     * queue of MIB files to be loaded, unless it is already loaded 
+     * queue of MIB files to be loaded, unless it is already loaded
      * or in the queue. The file is searched for in the MIB search
      * path.
-     * 
+     *
      * @param name           the MIB name (filename without extension)
-     * 
-     * @throws FileNotFoundException if the MIB file couldn't be 
+     *
+     * @throws FileNotFoundException if the MIB file couldn't be
      *             found in the MIB search path
      */
     void scheduleLoad(String name) throws FileNotFoundException {
         MibSource  src;
-        
+
         src = locate(name);
         if (src == null) {
-            throw new FileNotFoundException("couldn't locate MIB: '" + 
+            throw new FileNotFoundException("couldn't locate MIB: '" +
                                             name + "'");
         }
         scheduleLoad(src);
@@ -354,9 +437,9 @@ public class MibLoader {
 
     /**
      * Schedules the loading of a MIB file. The file is added to the
-     * queue of MIB files to be loaded, unless it is already loaded 
+     * queue of MIB files to be loaded, unless it is already loaded
      * or in the queue.
-     * 
+     *
      * @param file           the MIB file
      */
     void scheduleLoad(File file) {
@@ -367,7 +450,7 @@ public class MibLoader {
      * Schedules the loading of a MIB. The MIB source is added to the
      * queue of MIB:s to be loaded, unless it is already loaded or in
      * the queue.
-     * 
+     *
      * @param src            the MIB source
      */
     void scheduleLoad(MibSource src) {
@@ -378,13 +461,13 @@ public class MibLoader {
 
     /**
      * Loads all MIB files in the loader queue. New entries may be
-     * added to the queue while loading a MIB, as a result of 
+     * added to the queue while loading a MIB, as a result of
      * importing other MIB files. This method will either load all
      * MIB files in the queue or none (if errors were encountered).
-     * 
+     *
      * @return the loader log for the whole queue
-     * 
-     * @throws FileNotFoundException if the MIB file in the queue 
+     *
+     * @throws FileNotFoundException if the MIB file in the queue
      *             couldn't be found
      */
     private MibLoaderLog loadQueue() throws FileNotFoundException {
@@ -398,7 +481,7 @@ public class MibLoader {
                 src = (MibSource) queue.get(0);
                 processed.add(src.createMib(this, log));
             } catch (MibLoaderException e) {
-                // Do nothing, errors are already in the log 
+                // Do nothing, errors are already in the log
             }
             queue.remove(0);
         }
@@ -409,7 +492,7 @@ public class MibLoader {
             try {
                 ((Mib) processed.get(i)).initialize();
             } catch (MibLoaderException e) {
-                // Do nothing, errors are already in the log 
+                // Do nothing, errors are already in the log
             }
         }
 
@@ -418,7 +501,7 @@ public class MibLoader {
             try {
                 ((Mib) processed.get(i)).validate();
             } catch (MibLoaderException e) {
-                // Do nothing, errors are already in the log 
+                // Do nothing, errors are already in the log
             }
         }
 
@@ -426,43 +509,43 @@ public class MibLoader {
         if (log.errorCount() > 0) {
             mibs.removeAll(processed);
         }
-        
+
         return log;
     }
 
 
     /**
-     * A MIB input source. This class encapsulates the two different 
+     * A MIB input source. This class encapsulates the two different
      * ways of loacating a MIB file, either through a file or a URL.
      */
     private class MibSource {
-        
+
         /**
          * The MIB file. This variable must always be set, as it is
          * used for printing the MIB name.
          */
         private File file = null;
-        
+
         /**
          * The MIB URL location. This variable is only set if the MIB
          * is read from a URL.
          */
         private URL url = null;
-        
+
         /**
          * Creates a new MIB input source. The MIB will be read from
          * the specified file.
-         * 
+         *
          * @param file           the file to read from
          */
         public MibSource(File file) {
             this.file = file;
         }
-        
+
         /**
          * Creates a new MIB input source. The MIB will be read from
          * the specified URL.
-         * 
+         *
          * @param name           the MIB name
          * @param url            the URL to read from
          */
@@ -470,14 +553,14 @@ public class MibLoader {
             this.file = new File(name);
             this.url = url;
         }
-        
+
         /**
-         * Checks if this object is equal to another. This method 
+         * Checks if this object is equal to another. This method
          * will only return true for another mib source object with
          * the same input source.
-         * 
+         *
          * @param obj            the object to compare with
-         * 
+         *
          * @return true if the object is equal to this, or
          *         false otherwise
          */
@@ -498,9 +581,9 @@ public class MibLoader {
 
         /**
          * Returns the MIB file. If the MIB is loaded from URL this
-         * file does not actually exist, but is used for providing a 
+         * file does not actually exist, but is used for providing a
          * unique reference to the MIB.
-         * 
+         *
          * @return the MIB file
          */
         public File getFile() {
@@ -510,17 +593,17 @@ public class MibLoader {
         /**
          * Creates the MIB container. This method will read the MIB
          * either from file or from URL.
-         * 
+         *
          * @param loader         the MIB loader to use for imports
          * @param log            the MIB log to use for errors
-         * 
+         *
          * @return the MIB container created
-         * 
+         *
          * @throws FileNotFoundException if the MIB couldn't be found
-         * @throws MibLoaderException if the MIB couldn't be parsed 
+         * @throws MibLoaderException if the MIB couldn't be parsed
          *             or analyzed correctly
          */
-        public Mib createMib(MibLoader loader, MibLoaderLog log) 
+        public Mib createMib(MibLoader loader, MibLoaderLog log)
             throws FileNotFoundException, MibLoaderException {
 
             Reader  input;
@@ -541,20 +624,20 @@ public class MibLoader {
 
     /**
      * A MIB file name filter. This filter compares a file name with
-     * a MIB name, accepting only files with the MIB name. Any 
-     * extension on the file will be disregarded, but the name 
-     * comparison is made in a case-sensitive way. 
+     * a MIB name, accepting only files with the MIB name. Any
+     * extension on the file will be disregarded, but the name
+     * comparison is made in a case-sensitive way.
      */
     private class MibFileFilter implements FilenameFilter {
-    
+
         /**
          * The base MIB name.
          */
         private String basename;
-        
+
         /**
          * Creates a new MIB file filter.
-         * 
+         *
          * @param name           the MIB name to filter with
          */
         public MibFileFilter(String name) {
@@ -563,16 +646,16 @@ public class MibLoader {
 
         /**
          * Checks if a file name matches the MIB name.
-         * 
+         *
          * @param dir            the file directory
          * @param name           the file name
-         * 
-         * @return true if the file name matches, or 
+         *
+         * @return true if the file name matches, or
          *         false otherwise
          */
         public boolean accept(File dir, String name) {
-            return name.equals(basename) 
+            return name.equals(basename)
                 || name.startsWith(basename + ".");
         }
-    }    
+    }
 }
--- src/java/net/percederberg/mibble/MibLoaderException.java
+++ src/java/net/percederberg/mibble/MibLoaderException.java
@@ -33,13 +33,15 @@
 
 package net.percederberg.mibble;
 
+import java.io.File;
+
 /**
  * A MIB loader exception. This exception is thrown when a MIB file
- * couldn't be loaded properly, normally due to syntactical or 
- * semantical errors in the file. 
+ * couldn't be loaded properly, normally due to syntactical or
+ * semantical errors in the file.
  *
  * @author   Per Cederberg, <per at percederberg dot net>
- * @version  2.0
+ * @version  2.3
  * @since    2.0
  */
 public class MibLoaderException extends Exception {
@@ -51,25 +53,39 @@ public class MibLoaderException extends Exception {
 
     /**
      * Creates a new MIB loader exception.
-     * 
+     *
      * @param log            the MIB loader log
      */
     public MibLoaderException(MibLoaderLog log) {
         this.log = log;
     }
-    
+
+    /**
+     * Creates a new MIB loader exception. The specified message will
+     * be added to a new MIB loader log as an error.
+     *
+     * @param file           the file containg the error
+     * @param message        the error message
+     *
+     * @since 2.3
+     */
+    public MibLoaderException(File file, String message) {
+        log = new MibLoaderLog();
+        log.addError(file, -1, -1, message);
+    }
+
     /**
      * Returns the MIB loader log.
-     * 
+     *
      * @return the MIB loader log
      */
     public MibLoaderLog getLog() {
         return log;
     }
-    
+
     /**
      * Returns a error summary message.
-     * 
+     *
      * @return a error summary message
      */
     public String getMessage() {
