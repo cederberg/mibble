c1d48a54363812dd03b03056711f9d433ac58d21
Per Cederberg <per@percederberg.net>
2004-10-22 18:18:57 +0000

    Fixed Windows batch files for JAVA_HOME containing spaces.

--- src/bin/MibblePrinter.bat
+++ src/bin/MibblePrinter.bat
@@ -7,4 +7,4 @@ REM Set Mibble environment variables
 call .\setenv.bat
 
 REM Run Mibble MIB printer
-%JAVA_HOME%\bin\java net.percederberg.mibble.MibblePrinter %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JAVA_HOME%\bin\java" net.percederberg.mibble.MibblePrinter %1 %2 %3 %4 %5 %6 %7 %8 %9
--- src/bin/MibbleValidator.bat
+++ src/bin/MibbleValidator.bat
@@ -7,4 +7,4 @@ REM Set Mibble environment variables
 call .\setenv.bat
 
 REM Run Mibble MIB validator
-%JAVA_HOME%\bin\java net.percederberg.mibble.MibbleValidator %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JAVA_HOME%\bin\java" net.percederberg.mibble.MibbleValidator %1 %2 %3 %4 %5 %6 %7 %8 %9
