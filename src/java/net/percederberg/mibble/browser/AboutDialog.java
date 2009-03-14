016af8bf7a017de0a509523796d803459596bda3
Per Cederberg <per@percederberg.net>
2009-03-14 21:03:30 +0000

    Updated copyright year.

--- src/java/net/percederberg/mibble/browser/AboutDialog.java
+++ src/java/net/percederberg/mibble/browser/AboutDialog.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  *
- * Copyright (c) 2004 Per Cederberg. All rights reserved.
+ * Copyright (c) 2004-2009 Per Cederberg. All rights reserved.
  */
 
 package net.percederberg.mibble.browser;
@@ -57,7 +57,7 @@ public class AboutDialog extends JDialog {
      * The copyright text.
      */
     private static final String COPYRIGHT =
-        "(c) 2004 Per Cederberg. All rights reserved.\n" +
+        "(c) 2004-2009 Per Cederberg. All rights reserved.\n" +
         "This program comes with ABSOLUTELY NO\n" +
         "WARRANTY; for details see the accompanying\n" +
         "license.";
