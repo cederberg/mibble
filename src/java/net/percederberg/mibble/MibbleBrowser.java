849040336eda8159830846c4db3e577622a23e4b
Per Cederberg <per@percederberg.net>
2010-02-01 19:37:54 +0000

    Updated copyright.

--- src/java/net/percederberg/mibble/MibbleBrowser.java
+++ src/java/net/percederberg/mibble/MibbleBrowser.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  *
- * Copyright (c) 2004-2009 Per Cederberg. All rights reserved.
+ * Copyright (c) 2004-2010 Per Cederberg. All rights reserved.
  */
 
 package net.percederberg.mibble;
