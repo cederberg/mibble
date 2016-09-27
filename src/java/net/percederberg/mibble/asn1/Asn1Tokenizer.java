babd14423f1f52dd35eb03b57216cf0db2e26dc8
Per Cederberg <cederberg@gmail.com>
2016-09-27 14:19:20 +0200

    license: Removed GNU GPL license stanza from source file headers.
    Code is dual-licensed, which should be reflected in source headers.

--- src/java/net/percederberg/mibble/asn1/Asn1Tokenizer.java
+++ src/java/net/percederberg/mibble/asn1/Asn1Tokenizer.java
@@ -3,22 +3,9 @@
  *
  * THIS FILE HAS BEEN GENERATED AUTOMATICALLY. DO NOT EDIT!
  *
- * This work is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * See LICENSE.txt for licensing information.
  *
- * This work is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
- *
- * Copyright (c) 2004-2009 Per Cederberg. All rights reserved.
+ * Copyright (c) 2004-2016 Per Cederberg. All rights reserved.
  */
 
 package net.percederberg.mibble.asn1;
